85cf98de56136256f6342310a0462544d5343d49
=ht111111 <=3350727431@qq.com>
2021-04-20 16:53:11 +0800

    ”我的第二次提交

---
+++ src/views/user/user.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { fetchList } from '@/api/user'
+export default {
+    data(){
+        return{
+            list:null,
+        }
+    },
+    methods:{
+        getList(){
+            fetchList().then(response=>{
+                console.log(response);
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
